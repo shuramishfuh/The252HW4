67d09e0468be65c80896924c790032a3ab7fcfd4
Jadsy <jadsaad01@gmail.com>
2021-11-14 12:23:31 +0200

    implemented schedule

---
+++ src/Implementations/IMSchedule.java
@@ -0,0 +1,37 @@
+package Implementations;
+
+import java.time.LocalTime;
+
+public class IMSchedule implements Interfaces.Schedule{
+	private String Room;
+	private LocalTime FromTime;
+	private LocalTime ToTime;
+	private String Instructor;
+	private String Course;
+	IMSchedule(String room, LocalTime fromTime, LocalTime toTime, String instructor, String course) {
+		
+		this.Room = room;
+		this.FromTime = fromTime;
+		this.ToTime = toTime;
+		this.Instructor = instructor;
+		this.Course = course;
+	}
+	public String getRoom() {
+		return this.Room;
+	}
+	public LocalTime getFromTime() {
+		return this.FromTime;
+	}
+	public LocalTime getToTime() {
+		return this.ToTime;
+	}
+	public String getInstructor() {
+		return this.Instructor;
+	}
+	public String getCourse() {
+		return this.Course;
+	}
+
+	
+
+}
