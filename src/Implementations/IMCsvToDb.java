4c1392094a1dde450b7e10d1a6d1b759b02dc237
Jadsy <jadsaad01@gmail.com>
2021-11-14 13:48:38 +0200

    ignored courses with no start time

--- src/Implementations/IMCsvToDb.java
+++ src/Implementations/IMCsvToDb.java
@@ -16,15 +16,19 @@ public class IMCsvToDb implements Interfaces.CsvToDb {
 			String line = reader.readLine();
 			while (line != null) {
 				List<String> r = Arrays.asList(line.split("\\s*,\\s*"));
-				Course course = new IMCourse(r.get(0), r.get(1), r.get(2), r.get(3), r.get(4),
-						Float.parseFloat(r.get(5)), r.get(6), Integer.parseInt(r.get(7)), Integer.parseInt(r.get(8)),
-						r.get(9), r.get(10), r.get(11), r.get(12), !r.get(13).equals("."), !r.get(14).equals("."),
-						!r.get(15).equals("."), !r.get(16).equals("."), !r.get(17).equals("."), !r.get(18).equals("."),
-						r.get(19), r.get(20));
+				if(!r.get(10).equals(".")) {
+					
+					Course course = new IMCourse(r.get(0), r.get(1), r.get(2), r.get(3), r.get(4),
+							Float.parseFloat(r.get(5)), r.get(6), Integer.parseInt(r.get(7)), Integer.parseInt(r.get(8)),
+							r.get(9), r.get(10), r.get(11), r.get(12), !r.get(13).equals("."), !r.get(14).equals("."),
+							!r.get(15).equals("."), !r.get(16).equals("."), !r.get(17).equals("."), !r.get(18).equals("."),
+							r.get(19), r.get(20));
 
-				courses.add(course);
+					courses.add(course);
+				}
+				
 				line = reader.readLine();
-
+				
 			}
 			reader.close();
 		} catch (IOException e) {
