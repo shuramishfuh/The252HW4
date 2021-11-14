760f84f80f027de9385881f077d0c2bf6a7f84d9
Ramish <agamatimtim@gmail.com>
2021-11-14 14:57:39 +0200

    change Imschedule implementations from requiring room and instructors to strings

--- src/Implementations/IMCourSeera.java
+++ src/Implementations/IMCourSeera.java
@@ -1,4 +1,4 @@
-package Implementations;
+ package Implementations;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -13,82 +13,96 @@ import Interfaces.Instructor;
 import Interfaces.Room;
 import Interfaces.Schedule;
 
-public class IMCourSeera implements Interfaces.CourSeera{
-	private TreeMap<Room, List<Schedule>> roomSched;
-	IMCourSeera(List<Course> courses){
-		List<String> roomsCreated = new ArrayList<String>();
-		roomSched = new TreeMap<Room, List<Schedule>>();
-		for(Course c: courses) {
-			
-			Room r = new IMRoom(c.getBldg(), c.getRoom());
-			roomsCreated.add(c.getBldg()+" "+ c.getRoom());
-			Schedule s = new IMSchedule(
-						 new IMRoom(c.getBldg(), c.getRoom()),
-						 c.getBegin_time(),
-						 c.getEnd_time(),
-						 new IMInstructor(c.getInstructor_first(), c.getInstructor_last()),
-						 c.getTitle()
-					);
-			if(roomSched.isEmpty()||!roomsCreated.contains(c.getBldg()+" "+ c.getRoom())) {
-				List<Schedule> lista = Arrays.asList(s);
-				roomSched.put(null, lista);
-			}else {
-				roomSched.get(r).add(s);
-			}
-		}
-	}
-	@Override
-	public TreeMap<Room, List<Schedule>> roomSchedule() {
-		// TODO Auto-generated method stub
-		return this.roomSched;
-	}
+public class IMCourSeera implements Interfaces.CourSeera {
+    private TreeMap<Room, List<Schedule>> roomSched;
 
-	@Override
-	public List<Schedule> roomSchedule(Room room) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    IMCourSeera(List<Course> courses) {
+        List<String> roomsCreated = new ArrayList<String>();
+        roomSched = new TreeMap<Room, List<Schedule>>();
+        for (Course courese : courses) {
 
-	@Override
-	public List<Schedule> roomSchedule(Room room, LocalDate date) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        }
+        for (Course c : courses) {
+            if (c.getBldg().length() > 2 & c.getRoom().length() > 2) { // remove all none rooms
+                Room room = new IMRoom(c.getBldg(), c.getRoom());
+                Schedule s = new IMSchedule(
+                       (c.getBldg() +" "+ c.getRoom()),
+                        c.getBegin_time(),
+                        c.getEnd_time(),
+                        (c.getInstructor_first()+" "+ c.getInstructor_last()),
+                        c.getTitle()
+                );
+//               if (roomSched.isEmpty()){
+//                   roomSched.put(room,s);
+//               }
+                System.out.println(s.getRoom() + " " + s.getFromTime() + " " + s.getToTime() + " " + s.getInstructor() + " " + s.getCourse());
+//				System.out.println(roomsCreated);
+            }
 
-	@Override
-	public List<Schedule> roomSchedule(Room room, DayOfWeek day) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public Schedule whoWasThereLast(Room room) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//
+//
+//			if(roomSched.isEmpty()||!roomsCreated.contains(c.getBldg()+" "+ c.getRoom())) {
+//				List<Schedule> lista = Arrays.asList(s);
+//				roomSched.put(null, lista);
+//			}else {
+//				roomSched.get(r).add(s);
+//			}
+        }
+    }
 
-	@Override
-	public Schedule whoIsThereNow(Room room) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public TreeMap<Room, List<Schedule>> roomSchedule() {
+        // TODO Auto-generated method stub
+        return this.roomSched;
+    }
 
-	@Override
-	public List<Schedule> profSchedule(Instructor instructor) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Schedule> roomSchedule(Room room) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Schedule whereIsProf(Instructor instructor) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Schedule> roomSchedule(Room room, LocalDate date) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public List<Schedule> whereWillProfBe(Instructor instructor) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Schedule> roomSchedule(Room room, DayOfWeek day) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Schedule whoWasThereLast(Room room) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Schedule whoIsThereNow(Room room) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Schedule> profSchedule(Instructor instructor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Schedule whereIsProf(Instructor instructor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Schedule> whereWillProfBe(Instructor instructor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/Implementations/IMSchedule.java
+++ src/Implementations/IMSchedule.java
@@ -2,32 +2,38 @@ package Implementations;
 
 import java.time.LocalTime;
 
-public class IMSchedule implements Interfaces.Schedule{
-	private IMRoom Room;
-	private LocalTime FromTime;
-	private LocalTime ToTime;
-	private IMInstructor Instructor;
-	private String Course;
-	IMSchedule(IMRoom room, LocalTime fromTime, LocalTime toTime, IMInstructor instructor, String course) {
-		this.Room = room;
-		this.FromTime = fromTime;
-		this.ToTime = toTime;
-		this.Instructor = instructor;
-		this.Course = course;
-	}
-	public String getRoom() {
-		return this.Room.getBuilding()+ " " + this.Room.getRoomNumber();
-	}
-	public LocalTime getFromTime() {
-		return this.FromTime;
-	}
-	public LocalTime getToTime() {
-		return this.ToTime;
-	}
-	public String getInstructor() {
-		return this.Instructor.getFirstName() + " " + this.Instructor.getLastName();
-	}
-	public String getCourse() {
-		return this.Course;
-	}
+public class IMSchedule implements Interfaces.Schedule {
+    private String Room;
+    private LocalTime FromTime;
+    private LocalTime ToTime;
+    private String Instructor;
+    private String Course;
+
+    IMSchedule(String room, LocalTime fromTime, LocalTime toTime, String instructor, String course) {
+        this.Room = room;
+        this.FromTime = fromTime;
+        this.ToTime = toTime;
+        this.Instructor = instructor;
+        this.Course = course;
+    }
+
+    public String getRoom() {
+        return this.Room;
+    }
+
+    public LocalTime getFromTime() {
+        return this.FromTime;
+    }
+
+    public LocalTime getToTime() {
+        return this.ToTime;
+    }
+
+    public String getInstructor() {
+        return this.Instructor.getFirstName() + " " + this.Instructor.getLastName();
+    }
+
+    public String getCourse() {
+        return this.Course;
+    }
 }
