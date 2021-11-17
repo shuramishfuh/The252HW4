65da93054b151b5a397756a7ed8a3e7c1542b1d9
Fadi Hasaniyyah <fhh13@mail.aub.edu>
2021-11-17 18:47:01 +0200

    Removed the methods that download all HTML files at onec since it's not used

--- src/Implementations/HW4.java
+++ src/Implementations/HW4.java
@@ -10,47 +10,45 @@ public class HW4 implements Interfaces.HW4 {
 	public static void main(String[] args) {
 		List<Course> courses = new ArrayList<Course>();
 		listGenerator(courses);
-		CourSeeraFactory csf= new IMCourSeeraFactory();	
+		CourSeeraFactory csf = new IMCourSeeraFactory();
 		CourSeera CS = csf.createInstance(courses);
 
-//		IMInstructor imInstructor = new IMInstructor("Michel"," Kazan");
-//		IMRoom room = new IMRoom("PHYS","204");
-//		IMRoom room = new IMRoom("BIOL","SLH");
+		// IMInstructor imInstructor = new IMInstructor("Michel"," Kazan");
+		// IMRoom room = new IMRoom("PHYS","204");
+		// IMRoom room = new IMRoom("BIOL","SLH");
 
+		// Schedule s= CS.whoWasThereLast(room);
+		// List<Schedule> sc= CS.roomSchedule(room);
+		// List<Schedule> sc= CS.whereWillProfBe(imInstructor);
+		// List<Schedule> sc = CS.profSchedule(imInstructor);
+		// Schedule s = CS.whereIsProf(imInstructor);
+		// Schedule s = CS.whoIsThereNow(room);
+		// System.out.println(sc);
+		// for (Schedule s : sc
+		// ) {
+		// System.out.println(s.getInstructor());
+		// System.out.println(s.getCourse());
+		// System.out.println(s.getFromTime());
+		// System.out.println(s.getToTime());
+		// System.out.println(s.getRoom());
+		// System.out.println(s.getDay());
+		// }
 
-
-//		 Schedule s= CS.whoWasThereLast(room);
-//		 List<Schedule> sc= CS.roomSchedule(room);
-//		 List<Schedule> sc= CS.whereWillProfBe(imInstructor);
-//		 List<Schedule> sc = CS.profSchedule(imInstructor);
-//		 Schedule s = CS.whereIsProf(imInstructor);
-//		Schedule s = CS.whoIsThereNow(room);
-//		System.out.println(sc);
-//		for (Schedule s : sc
-//			 ) {
-//			System.out.println(s.getInstructor());
-//			System.out.println(s.getCourse());
-//			System.out.println(s.getFromTime());
-//			System.out.println(s.getToTime());
-//			System.out.println(s.getRoom());
-//			System.out.println(s.getDay());
-//		}
-
-//		for (Room room : CS.roomSchedule().keySet()) {
-//			List<Schedule> a = CS.roomSchedule().get(room);
-//			for (Schedule s: a
-//			) {
-//				System.out.println(s.getInstructor());
-//				System.out.println(s.getCourse());
-//				System.out.println(s.getRoom());
-//				System.out.println(s.getFromTime());
-//				System.out.println(s.getToTime());
-//				System.out.println(s.getDay());
-//				System.out.println();
-//				System.out.println();
-//				System.out.println();
-//			}
-//		}
+		// for (Room room : CS.roomSchedule().keySet()) {
+		// List<Schedule> a = CS.roomSchedule().get(room);
+		// for (Schedule s: a
+		// ) {
+		// System.out.println(s.getInstructor());
+		// System.out.println(s.getCourse());
+		// System.out.println(s.getRoom());
+		// System.out.println(s.getFromTime());
+		// System.out.println(s.getToTime());
+		// System.out.println(s.getDay());
+		// System.out.println();
+		// System.out.println();
+		// System.out.println();
+		// }
+		// }
 
 	}
 
@@ -71,8 +69,8 @@ public class HW4 implements Interfaces.HW4 {
 		System.out.println("Starting!");
 		for (char a : letters) {
 			htmlLink = String.valueOf(a).concat(".html");
-//			downloader.downloadHtmlToFile(pre.concat(String.valueOf(a)).concat(".htm"), htmlLink);
-//			System.out.println("Page for letter " + String.valueOf(a) + " has been downloaded");
+			downloader.downloadHtmlToFile(pre.concat(String.valueOf(a)).concat(".htm"), htmlLink);
+			System.out.println("Page for letter " + String.valueOf(a) + " has been downloaded");
 			csvMaker.htmlToCsv(htmlLink, csvFileName);
 			System.out.println("Page for letter " + String.valueOf(a) + " has been parsed into csv");
 		}
--- src/Implementations/IMDownloader.java
+++ src/Implementations/IMDownloader.java
@@ -4,25 +4,25 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 
 public class IMDownloader implements Interfaces.Downloader {
 
     /*
-     * @param url  : url to download content from
+     * @param url : url to download content from
+     * 
      * @param file : file path to store downloaded content
      *
-     * downloadHtmlToFile take a url and a file path
-     * downloads html page from url and s
-     * */
+     * downloadHtmlToFile take a url and a file path downloads html page from url
+     * and s
+     */
     public void downloadHtmlToFile(String url, String file) {
         try {
             URL urL = new URL(url);
             ReadableByteChannel readableByteChannel = Channels.newChannel(urL.openStream());
             FileOutputStream fileOutputStream = new FileOutputStream(file);
-            fileOutputStream.getChannel()
-                    .transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            fileOutputStream.close();
         } catch (MalformedURLException e) {
 
             e.printStackTrace();
@@ -30,23 +30,5 @@ public class IMDownloader implements Interfaces.Downloader {
 
             e.printStackTrace();
         }
-    }
-
-    /*
-     * downloads all courses from aub
-     * dynamic course scheduler
-     * */
-
-    public void downloadAllHmlToFiles() {
-        System.out.println("start downloading pages");
-        String pre = new String("https://www-banner.aub.edu.lb/catalog/schd_");
-        char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',};
-        for (char a : letters) {
-            downloadHtmlToFile(pre.concat(String.valueOf(a)).concat(".htm"),
-                    (String.valueOf(a).concat("_courses.html")));
-            System.out.println("done with " + String.valueOf(a) + " courses");
-        }
-        System.out.println("Done downloading pages");
     }
 }
