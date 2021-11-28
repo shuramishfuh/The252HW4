ccdfbacd78c59c89089116ed0bd532e686cf1420
Ramish <agamatimtim@gmail.com>
2021-11-28 10:05:19 +0200

    implemented InputParser Functor method  but can integrate to the interface

--- src/main/java/ImplementationFormatterEngine/InputParser.java
+++ src/main/java/ImplementationFormatterEngine/InputParser.java
@@ -4,6 +4,7 @@ import InterfacesformatterEngine.IinputParser;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class InputParser implements IinputParser {
@@ -12,4 +13,8 @@ public class InputParser implements IinputParser {
     public List<String> convertStringToInstruction(String str) {
         return Arrays.stream(str.split(" ")).collect(Collectors.toList());
     }
+
+    public Function<String, List<String>> convertStringToInstruction =str ->
+        Arrays.stream(str.split(" ")).collect(Collectors.toList());
+
 }
--- src/main/java/InterfacesformatterEngine/IinputParser.java
+++ src/main/java/InterfacesformatterEngine/IinputParser.java
@@ -1,6 +1,7 @@
 package InterfacesformatterEngine;
 
 import java.util.List;
+import java.util.function.Function;
 
 public interface IinputParser {
     List<String>  convertStringToInstruction (String str);
