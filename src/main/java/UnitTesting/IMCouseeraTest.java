cd3baeeda19c3d5f350ad65c3bf64097af3237f7
Fadi Hasaniyyah <fhh13@mail.aub.edu>
2021-11-29 13:59:52 +0200

    Started Unit Testing

---
+++ src/main/java/UnitTesting/IMCouseeraTest.java
@@ -0,0 +1,15 @@
+package UnitTesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class IMCouseeraTest {
+    
+    @Test
+    public void test(){
+
+    }
+
+}
