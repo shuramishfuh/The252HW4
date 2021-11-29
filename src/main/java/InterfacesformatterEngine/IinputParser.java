56d19a62eb9b55a7d797a6a2bddd2587df42936c
Fadi Hasaniyyah <fhh13@mail.aub.edu>
2021-11-29 19:56:36 +0200

    Minor syntax changes

--- src/main/java/InterfacesformatterEngine/IinputParser.java
+++ src/main/java/InterfacesformatterEngine/IinputParser.java
@@ -1,7 +1,6 @@
 package InterfacesformatterEngine;
 
 import java.util.List;
-import java.util.function.Function;
 
 public interface IinputParser {
     List<String>  convertStringToInstruction (String str);
