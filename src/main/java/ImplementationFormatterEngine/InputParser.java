19c565072eef6ce8a1d0cea2343d3be631aa9fa8
Fadi Hasaniyyah <fhh13@mail.aub.edu>
2021-11-30 19:59:56 +0200

    Added feature to handled Instructor's first/last name that is made up of two words by using "-" and updated help command accordingly

--- src/main/java/ImplementationFormatterEngine/InputParser.java
+++ src/main/java/ImplementationFormatterEngine/InputParser.java
@@ -12,9 +12,9 @@ public class InputParser implements IinputParser {
     @Override
     public List<String> convertStringToInstruction(String str) {
         List<String> list = new ArrayList<String>();
-        Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(str);
+        Matcher m = Pattern.compile("([^\"]\\S*|\".+?\"|[^-])\\s*").matcher(str);
         while (m.find())
-            list.add(m.group(1).replace("\"", ""));
+            list.add(m.group(1).replaceAll("\"|-", " "));
         return list;
     }
 }
